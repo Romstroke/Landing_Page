7847850d617630e88a3e8efa6fd4a71ae2f2f9ff
Romina <rominchuela@gmail.com>
2024-02-02 01:34:26 -0300

    Agrego tooltips customizados

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// tooltips 
+
+var tooltipTriggerList = document.querySelectorAll(
+	'[data-bs-toggle="tooltip"]'
+);
+var tooltipList = [...tooltipTriggerList].map(
+	(tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
