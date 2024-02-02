6eebb8964b77ab8ba7480393f89ddb1a546afdfe
Romina <rominchuela@gmail.com>
2024-02-02 16:42:17 -0300

    Cambio smooth scroll de css por jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ var tooltipList = [...tooltipTriggerList].map(
 	(tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
+//smooth scroll con jquery
+
+$("a").click(function () {
+	if (this.hash !== "") {
+		var hash = this.hash;
+		$("html, body").animate({
+			scrollTop: $(hash).offset().top
+		}, 250); // 1000 me parecía muy lento
+	}
+});
