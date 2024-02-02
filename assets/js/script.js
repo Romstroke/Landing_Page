754896e2ad89562198d82a9bdc7cbb5ebd12b4c9
Romina <rominchuela@gmail.com>
2024-02-02 17:27:35 -0300

    Agrego transición de color por scroll  al navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,19 @@ $("a").click(function () {
 		}, 250); // 1000 me parecía muy lento
 	}
 });
+
+// para que el navbar cambie de color al bajar 
+
+// seleccionamos al nav desde la clase .navbar
+var navbar = document.querySelector('.navbar');
+//escuchamos el evento scroll de la ventana que contiene nuestro DOM
+window.addEventListener('scroll', function () {
+	// cuando el scroll en el eje vertical sea mayor a 500 pixeles, 
+	//agregaremos al nav la clase .navbar-scrolled que creamos en style.css
+	if (window.scrollY > 500) {
+		navbar.classList.add('navbar-scrolled');
+	// y aqui removemos esa clase .navbar-scrolled para que vuelva a ser transparente	
+	} else if (window.scrollY <= 500) {
+		navbar.classList.remove('navbar-scrolled');
+	}
+});
